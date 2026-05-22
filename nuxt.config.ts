3f9b62a97b855241beeb6b7b0759d3d7f124358b
babanazar <babanazarjumadurdyyev@gmail.com>
2026-05-22 18:17:21 +0300

    fix: update default API URL to Render

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -61,7 +61,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:3001/api/v1'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://chinabox-api.onrender.com/api/v1'
     }
   },
 
